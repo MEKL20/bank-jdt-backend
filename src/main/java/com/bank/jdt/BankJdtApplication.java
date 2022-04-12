cf5d623a5d818aef1af958b3a23ba9ef98f2569d
Ricky Miftahuddin <rickymiftahuddin12@gmail.com>
2022-04-12 21:51:21 +0700

    fix package, change port to 5000

---
+++ src/main/java/com/bank/jdt/BankJdtApplication.java
@@ -0,0 +1,14 @@
+package com.bank.jdt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankJdtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankJdtApplication.class, args);
+        System.out.println("Springboot sudah berjalan...");
+    }
+
+}
