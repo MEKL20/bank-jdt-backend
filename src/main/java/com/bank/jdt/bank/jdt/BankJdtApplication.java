3f9111d4591ae0b6b76ec1ca08d23819d1aed506
MEKL20 <edwinkhoirudin16@gmail.com>
2022-04-12 17:35:56 +0700

    upload

---
+++ src/main/java/com/bank/jdt/bank/jdt/BankJdtApplication.java
@@ -0,0 +1,13 @@
+package com.bank.jdt.bank.jdt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankJdtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankJdtApplication.class, args);
+	}
+
+}
