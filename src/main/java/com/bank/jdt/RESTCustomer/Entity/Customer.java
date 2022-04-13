067e271b7513097bc4bd5c315e460a99f14ca641
Ricky Miftahuddin <rickymiftahuddin12@gmail.com>
2022-04-13 09:51:55 +0700

    add sample crud, hibernate, connect to database online, add more structure

--- src/main/java/com/bank/jdt/RESTCustomer/Entity/Customer.java
+++ src/main/java/com/bank/jdt/RESTCustomer/Entity/Customer.java
@@ -1,4 +1,28 @@
 package com.bank.jdt.RESTCustomer.Entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
 public class Customer {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private int id;
+    private String name;
+
+    public Customer() {
+    }
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+
 }
--- src/main/java/com/bank/jdt/RESTCustomer/Service/CustomerService.java
+++ src/main/java/com/bank/jdt/RESTCustomer/Service/CustomerService.java
@@ -1,4 +1,17 @@
 package com.bank.jdt.RESTCustomer.Service;
 
+import com.bank.jdt.RESTCustomer.Entity.Customer;
+import com.bank.jdt.RESTCustomer.Repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CustomerService {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    public Customer addCustomer(Customer customer){
+        return customerRepository.save(customer);
+    }
 }
