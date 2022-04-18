30cabd0b4abf20b5e63fd0bc37f06a1c06c9a8fe
MEKL20 <edwinkhoirudin16@gmail.com>
2022-04-18 12:53:48 +0700

    update response api

--- src/main/java/com/bank/jdt/RESTSaving/Service/SavingService.java
+++ src/main/java/com/bank/jdt/RESTSaving/Service/SavingService.java
@@ -4,6 +4,7 @@ import com.bank.jdt.RESTReporting.Service.ReportingService;
 import com.bank.jdt.RESTCustomer.Entity.Customer;
 import com.bank.jdt.RESTSaving.Entity.Saving;
 import com.bank.jdt.RESTSaving.Repository.SavingRepository;
+import net.minidev.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -39,60 +40,90 @@ public class SavingService {
         return savingRepository.findByUsername(username);
     }
 
-    public ResponseEntity<String> savingWithdraw(String username, Saving saving){
+    public ResponseEntity savingWithdraw(String username, Saving saving){
+        JSONObject res= new JSONObject();
+        JSONObject result = new JSONObject();
         try{
             if (saving.getBalance() < 10000){
-                return new ResponseEntity<>("Minimum Withdraw 10,000", HttpStatus.BAD_REQUEST);
+                res.put("message","Minimum Withdraw 10,000");
+                return new ResponseEntity(res, HttpStatus.BAD_REQUEST);
             }
             Saving accountSaving = savingRepository.findByUsername(username);
             if ((accountSaving.getBalance() - saving.getBalance()) < 0){
-                return new ResponseEntity<>("Balance Is Not Enough", HttpStatus.BAD_REQUEST);
+                res.put("message","Balance Is Not Enough");
+                return new ResponseEntity<>(res, HttpStatus.BAD_REQUEST);
             }
             accountSaving.setBalance(accountSaving.getBalance() - saving.getBalance());
             savingRepository.save(accountSaving);
             reportingService.reportingWithdraw(accountSaving, saving);
-            return new ResponseEntity<>("Withdraw Success", HttpStatus.OK);
+            result.put("username", accountSaving.getCustomer().getUsername());
+            result.put("accountSaving", accountSaving.getAccountSaving());
+            result.put("balance", accountSaving.getBalance());
+            res.put("message","Withdraw "+saving.getBalance()+" Success");
+            res.put("result", result);
+            return new ResponseEntity(res, HttpStatus.OK);
         }catch (Exception err){
-            return new ResponseEntity<>(err.getMessage(), HttpStatus.BAD_GATEWAY);
+            res.put("message",err.getMessage());
+            return new ResponseEntity(res, HttpStatus.BAD_GATEWAY);
         }
     }
 
-    public ResponseEntity<String> savingTopup(String username, Saving saving){
+    public ResponseEntity savingTopup(String username, Saving saving){
+        JSONObject res= new JSONObject();
+        JSONObject result = new JSONObject();
         try {
             if (saving.getBalance() < 10000){
-                return new ResponseEntity<>("Minimum TopUp 10,000", HttpStatus.BAD_REQUEST);
+                res.put("message","Minimum TopUp 10,000");
+                return new ResponseEntity(res, HttpStatus.BAD_REQUEST);
             }
             Saving accountSaving = savingRepository.findByUsername(username);
             accountSaving.setBalance(accountSaving.getBalance() + saving.getBalance());
             savingRepository.save(accountSaving);
             reportingService.reportingTopup(accountSaving, saving);
-            return new ResponseEntity<>("TopUp Success",HttpStatus.OK);
+            result.put("username", accountSaving.getCustomer().getUsername());
+            result.put("accountSaving", accountSaving.getAccountSaving());
+            result.put("balance", accountSaving.getBalance());
+            res.put("message","TopUp "+saving.getBalance()+" Success");
+            res.put("result", result);
+            return new ResponseEntity(res,HttpStatus.OK);
         }catch (Exception err){
-            return new ResponseEntity<>(err.getMessage(), HttpStatus.BAD_GATEWAY);
+            res.put("message",err.getMessage());
+            return new ResponseEntity(res, HttpStatus.BAD_GATEWAY);
         }
     }
 
-    public ResponseEntity<String> savingTransfer(String username, Saving saving){
+    public ResponseEntity savingTransfer(String username, Saving saving){
+        JSONObject res= new JSONObject();
+        JSONObject result = new JSONObject();
         try {
             if (saving.getBalance() < 10000){
-                return new ResponseEntity<>("Minimum Transfer 10,000", HttpStatus.BAD_REQUEST);
+                res.put("message","Minimum Transfer 10,000");
+                return new ResponseEntity(res, HttpStatus.BAD_REQUEST);
             }
             Saving source = savingRepository.findByUsername(username);
             Saving destination = savingRepository.findByAccountSaving(saving.getAccountSaving());
             if (destination == null) {
-                return new ResponseEntity<>("Account Saving Destination Not Found", HttpStatus.BAD_REQUEST);
+                res.put("message","Account Saving Destination Not Found");
+                return new ResponseEntity(res, HttpStatus.BAD_REQUEST);
             }
             if ((source.getBalance() - saving.getBalance()) < 0) {
-                return new ResponseEntity<>("Balance Is Not Enough", HttpStatus.BAD_REQUEST);
+                res.put("message","Balance Is Not Enough");
+                return new ResponseEntity(res, HttpStatus.BAD_REQUEST);
             }
             source.setBalance(source.getBalance() - saving.getBalance());
             savingRepository.save(source);
             destination.setBalance(destination.getBalance() + saving.getBalance());
             savingRepository.save(destination);
             reportingService.reportingTransfer(source, saving.getBalance(), destination);
-            return new ResponseEntity<>("Transfer Success", HttpStatus.OK);
+            result.put("username", source.getCustomer().getUsername());
+            result.put("accountSaving", source.getAccountSaving());
+            result.put("balance", source.getBalance());
+            res.put("message","Transfer "+saving.getBalance()+"To "+destination.getAccountSaving()+" Success");
+            res.put("result", result);
+            return new ResponseEntity(res, HttpStatus.OK);
         }catch (Exception err){
-            return new ResponseEntity<>(err.getMessage(), HttpStatus.BAD_GATEWAY);
+            res.put("message",err.getMessage());
+            return new ResponseEntity(res, HttpStatus.BAD_GATEWAY);
         }
     }
 
